ecde3134d1d97d9b41425fccd810b98ac71b455a
hackkey04 <hashifatulkarimah23@gmail.com>
2026-09-24 15:14:32 +0700

    [P-5] migrasi semua tabel dan crud books categories selesai & tugas

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -12,7 +12,7 @@ class BookController extends Controller
 {
     public function index()
     {
-        $books = Book::paginate(10);ks;
+        $books = Book::paginate(10);
 
         return view('books.index', compact('books'));
     }
